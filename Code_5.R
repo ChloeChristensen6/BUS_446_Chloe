f8240d9cee631b4a8234182b8ae7d0dd1d968a77
ChloeChristensen6 <152525932+ChloeChristensen6@users.noreply.github.com>
2023-11-30 12:51:46 -0500

    Update and rename Code_5.R to Visualization and Final Findings

--- Code_5.R
+++
@@ -1,6 +0,0 @@
-ggplot(chloe_data, aes(x = weekday, y = hours)) +
-  geom_bar(colour="blue", fill="#DD8888", width=.8, stat="identity") +
-   guides(fill=FALSE) +
-  xlab("Weekday") + ylab("Hours") +
-  ggtitle("Peak Hours by Day of the Week") +
-   theme(plot.title = element_text(hjust = .5))
