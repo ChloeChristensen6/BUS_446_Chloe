05e6a38965ca1d2882941e851e4ed2460857a4ec
ChloeChristensen6 <152525932+ChloeChristensen6@users.noreply.github.com>
2023-11-30 12:50:48 -0500

    Update Code_1.R

--- Code_1.R
+++ Code_1.R
@@ -20,3 +20,10 @@ bike_usage = bike_usage [order(bike_usage$weekday),]
 library(ggplot2)
 chloe_data <- chloe_data %>%
   mutate(hours = seconds(usage) / 3600)
+
+ggplot(chloe_data, aes(x = weekday, y = hours)) +
+  geom_bar(colour="blue", fill="#DD8888", width=.8, stat="identity") +
+   guides(fill=FALSE) +
+  xlab("Weekday") + ylab("Hours") +
+  ggtitle("Peak Hours by Day of the Week") +
+   theme(plot.title = element_text(hjust = .5))
