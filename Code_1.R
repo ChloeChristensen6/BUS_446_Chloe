203d89143eb2908ed52e386c01cb0977b9b9cbde
ChloeChristensen6 <152525932+ChloeChristensen6@users.noreply.github.com>
2023-11-30 12:50:04 -0500

    Update Code_1.R

--- Code_1.R
+++ Code_1.R
@@ -5,3 +5,14 @@ data$weekday = weekdays(data$begin)
 data$hours = hours(data$begin)
 bike_hours = data %>% group_by(weekday) %>% summarize (hours = sum(duration)) 
 data$hours = as.POSIXct(data$started_at, format = "%m/%d/%Y %H:%M") 
+
+library(ggplot2)
+ggplot(bike_usage, aes(x = weekday, y = usage)) +
+  geom_bar(stat = "identity") 
++
+  labs(title = "Peak Hours by Day of the Week",
+       x = "Day of the Week",
+       y = "Peak Hour") +
+  theme_minimal()
+bike_usage$weekday = sort(bike_usage$weekday)
+bike_usage = bike_usage [order(bike_usage$weekday),]
